08694d19f74fc999bc226ef1812e85c56e92a75e
Adam Hocini <adam.hocini.ah@icloud.com>
2023-07-11 11:56:39 +0200

    ex02

---
+++ Ex 2/untitled1/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+
+class NombreAleatoire {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int essais = 0;
+
+        while (true) {
+            // Générer trois nombres aléatoires entre 0 et 1000
+            int nombre1 = random.nextInt(1001);
+            int nombre2 = random.nextInt(1001);
+            int nombre3 = random.nextInt(1001);
+            essais++; // Incrémenter le nombre d'essais à chaque itération
+
+            // Vérifier si les nombres forment la combinaison recherchée
+            if (nombre1 % 2 == 0 && nombre2 % 2 == 0 && nombre3 % 2 != 0) {
+                // System.out.println est utilisé pour imprimer un argument qui lui est passé
+                System.out.println("Combinaison trouvée !");
+                System.out.println("Nombre 1 : " + nombre1);
+                System.out.println("Nombre 2 : " + nombre2);
+                System.out.println("Nombre 3 : " + nombre3);
+                break; // Sortir de la boucle si la combinaison est trouvée
+            }
+        }
+
+        System.out.println("Nombre d'essais nécessaires : " + essais);
+    }
+}
