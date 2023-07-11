605326c893af1a578e2535a13e94ff494b7d8f00
Adam Hocini <adam.hocini.ah@icloud.com>
2023-07-11 09:49:29 +0200

    correction perso ex1

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        // Définition du tableau contenant les adresses e-mail
         String[] emails = {
                 "fzne@gmail.com",
                 "zejnfzj@sfr.com",
@@ -13,25 +14,34 @@ public class Main {
                 "didier@gmail.com"
         };
 
+        // Compteur pour le nombre total d'e-mails
         int totalEmails = emails.length;
+
+        // Compteurs pour les fournisseurs de services de messagerie
         int gmailCount = 0;
         int sfrCount = 0;
         int orangeCount = 0;
 
+        // Parcours de chaque adresse e-mail dans le tableau
         for (String email : emails) {
             if (email.endsWith("@gmail.com")) {
+                // Si l'adresse se termine par "@gmail.com", on incrémente le compteur pour Gmail
                 gmailCount++;
             } else if (email.endsWith("@sfr.com")) {
+                // Si l'adresse se termine par "@sfr.com", on incrémente le compteur pour SFR
                 sfrCount++;
             } else if (email.endsWith("@orange.com")) {
+                // Si l'adresse se termine par "@orange.com", on incrémente le compteur pour Orange
                 orangeCount++;
             }
         }
 
+        // Calcul des pourcentages pour chaque fournisseur
         double gmailPercentage = (double) gmailCount / totalEmails * 100;
         double sfrPercentage = (double) sfrCount / totalEmails * 100;
         double orangePercentage = (double) orangeCount / totalEmails * 100;
 
+        // Affichage des pourcentages correspondants
         System.out.println("Pourcentage de fournisseur de service mail :");
         System.out.println("Gmail : " + gmailPercentage + "%");
         System.out.println("SFR : " + sfrPercentage + "%");
